b0a74357983932c892754f704f7a758d60742a05
Brian Tan <britan3333@gmail.com>
2018-06-20 16:24:17 -0400

    food-page/new-banner-design: added new folders and image urls

--- brian-tan/src/assets/imageUrls.js
+++ brian-tan/src/assets/imageUrls.js
@@ -6,4 +6,20 @@ const images = {
     miscBanner: 'https://images.pexels.com/photos/924824/pexels-photo-924824.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
 };
 
+const foodBannerImages = {
+    icon1: 'https://images.pexels.com/photos/884600/pexels-photo-884600.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+    icon1: 'https://images.pexels.com/photos/262897/pexels-photo-262897.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+    icon1: 'https://images.pexels.com/photos/5916/food-salad-healthy-colorful.jpg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+    icon1: 'https://images.pexels.com/photos/5619/plate-white-fish-chilli.jpg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+    icon1: 'https://images.pexels.com/photos/660282/pexels-photo-660282.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+};
+
+const foodBannerImagesAlt = {
+    icon1: 'https://images.pexels.com/photos/59999/raspberries-fruits-fruit-berries-59999.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+    icon1: 'https://images.pexels.com/photos/255469/pexels-photo-255469.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+    icon1: 'https://images.pexels.com/photos/208453/pexels-photo-208453.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+    icon1: 'https://cdn.pixabay.com/photo/2016/05/12/16/34/ginger-1388002_960_720.jpg',
+    icon1: 'https://images.pexels.com/photos/63596/fruit-pepper-vegetable-vegetables-63596.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+};
+
 export default images;
