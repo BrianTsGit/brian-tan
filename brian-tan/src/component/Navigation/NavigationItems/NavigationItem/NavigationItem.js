160fc709c8a48f92318a485e7a1947c1dfb8af81
Brian Tan <britan3333@gmail.com>
2018-07-13 16:36:29 -0400

    consolidate-app/about-page-styles: added mobile styling to about page. implemented sidedrawer closing when navigating.

--- brian-tan/src/component/Navigation/NavigationItems/NavigationItem/NavigationItem.js
+++ brian-tan/src/component/Navigation/NavigationItems/NavigationItem/NavigationItem.js
@@ -6,7 +6,7 @@ import classes from './NavigationItem.scss';
 
 const navigationItem = (props) => {
     return (
-        <li className={classes.NavigationItem}>
+        <li className={classes.NavigationItem} onClick={props.clicked}>
             <NavLink to={props.link}>
                 {props.sectionName}
             </NavLink>
