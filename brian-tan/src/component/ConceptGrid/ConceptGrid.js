584c60877e83f0ec27e0eeae2c1c77efd341d402
Brian Tan <britan3333@gmail.com>
2018-07-24 16:48:57 -0400

    general/more-tool-bar: added logic to show concept image when hovering over text.

--- brian-tan/src/component/ConceptGrid/ConceptGrid.js
+++ brian-tan/src/component/ConceptGrid/ConceptGrid.js
@@ -1,69 +1,71 @@
 import React, { Component } from 'react';
 
 import classes from './ConceptGrid.scss';
-import GridImage from './GridImage/GridImage';
+import ConceptGridImage from './GridImage/GridImage';
 
-const conceptGrid = (props) => {
-    let data = [
-        {
-            text: 'GA',
-            image: 'https://images.pexels.com/photos/936112/pexels-photo-936112.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
-            url: 'https://www.gq.com/?us_site=y'
-        },
-        {
-            text: 'Montage Magazine',
-            image: 'https://images.pexels.com/photos/69212/pexels-photo-69212.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
-            url: 'https://wovenmagazine.com/'
-        }
-    ];
+class ConceptGrid extends Component {
+    state = {
+        concepts: [
+            {
+                text: 'Montage Magazine',
+                image: 'https://images.pexels.com/photos/936112/pexels-photo-936112.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+                url: 'https://wovenmagazine.com/'
+            },
+            {
+                text: 'GA',
+                image: 'https://images.pexels.com/photos/69212/pexels-photo-69212.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+                url: 'https://www.gq.com/?us_site=y'
+            }
+        ],
+        conceptGridImage: null
+    };
 
-    let conceptsGridClasses = [classes.ConceptsGrid, classes.GridClosed].join(' ');
-
-    if (props.showConceptsGrid) {
-        conceptsGridClasses = [classes.ConceptsGrid, classes.GridOpened].join(' ');
+    onConceptCellMouseEnterHandler = (conceptGridImage) => {
+        console.log('conceptGridImage', conceptGridImage);
+        this.setState({ conceptGridImage: conceptGridImage });
     }
 
-    let gridCells = data.map(cell => {
-        return (
-            <div key={cell.text} className={classes.ConceptCell}>
-                <a href={cell.url}>
-                    <span>{cell.text}</span>
+    onConceptCellMouseLeaveHandler = () => {
+        this.setState({ conceptGridImage: null });
+    }
+
+    render () {
+        let conceptsGridClasses = [classes.ConceptsGrid, classes.GridClosed].join(' ');
+
+        if (this.props.showConceptsGrid) {
+            conceptsGridClasses = [classes.ConceptsGrid, classes.GridOpened].join(' ');
+        }
+
+        let conceptCells = this.state.concepts.map(concept => {
+            return (
+                <a key={concept.text} href={concept.url}>
+                    <div 
+                        className={classes.ConceptCell}
+                        onMouseEnter={() => this.onConceptCellMouseEnterHandler(concept.image)}
+                        onMouseLeave={this.onConceptCellMouseLeaveHandler}>
+                            <span>{concept.text}</span>  
+                    </div>
                 </a>
-            </div>
-        )
-    })
+            )
+        })
 
-    return (
+        return (
 
-        <div className={conceptsGridClasses}>
-            <div className={classes.ImageCover}>
-                <GridImage image="https://images.pexels.com/photos/936112/pexels-photo-936112.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" />
-                {/* <GridImage image="https://images.pexels.com/photos/69212/pexels-photo-69212.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" /> */}
-            </div>
-            <div className={classes.GridHeader}>
-                <div onClick={props.closeConceptsGrid} className={classes.CloseButton}>
-                    <i className="fas fa-times"/>
+            <div className={conceptsGridClasses}>
+                <div className={classes.ImageCover}>
+                    <ConceptGridImage show image={this.state.conceptGridImage} />
+                </div>
+                <div className={classes.GridHeader}>
+                    <div onClick={this.props.closeConceptsGrid} className={classes.CloseButton}>
+                        <i className="fas fa-times"/>
+                    </div>
+                </div>
+                <div className={classes.CellContainer}>
+                    {conceptCells}
                 </div>
             </div>
-            <div className={classes.CellContainer}>
-                <div className={classes.ConceptCell}>
-                    <span>GA</span>
-                </div>
-                <div className={classes.ConceptCell}>
-                    <span>taste</span>
-                </div>
-                <div className={classes.ConceptCell}>
-                    <span>Montage Magazine</span>
-                </div>
-                <div className={classes.ConceptCell}>
-                    <span>Thrill</span>
-                </div>
-                <div className={classes.ConceptCell}>
-                    <span>Movieman</span>
-                </div>
-            </div>
-        </div>
-    );
+        );
+    }
 }
 
-export default conceptGrid;
+export default ConceptGrid;
--- brian-tan/src/component/ConceptGrid/GridImage/GridImage.js
+++ brian-tan/src/component/ConceptGrid/GridImage/GridImage.js
@@ -3,6 +3,12 @@ import React from 'react';
 import classes from './GridImage.scss';
 
 const gridImage = (props) => {
+    let gridImageClasses = [classes.GridImage, classes.Hide].join(' ');
+
+    if (props.show) {
+        gridImageClasses = [classes.GridIamge, classes.Show].join(' ');
+    }
+
     return (
         <div className={classes.GridImage}
             style={{ backgroundImage: 'url(' + props.image + ')' }}>
