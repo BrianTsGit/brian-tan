31095dfad44dfa0974b231dcef5a49ef1392a4e9
Brian Tan <britan3333@gmail.com>
2018-07-27 22:15:22 -0400

    concepts/ga-magazine: created the headline feed container

--- brian-tan/src/container/FashionMagazine/FashionMagazine.js
+++ brian-tan/src/container/FashionMagazine/FashionMagazine.js
@@ -32,7 +32,7 @@ class FashionMagazine extends Component {
                 _id: 'titleCard1',
                 url: 'https://www.gq.com/story/underrated-exercises-according-to-personal-trainers',
                 image_url: 'https://media.gq.com/photos/5b5a2662aa6bf94608b2597c/3:4/w_240/Underrated-Exercises-GQ-2018-072618.jpg',
-                category: 'Fitness',
+                category: 'Really Long Category',
                 headline: 'The Gym\'s 3 Most Underrated Exercises, According to Personal Trainers',
                 creator: 'CLAY SKIPPER'
             },
@@ -72,7 +72,64 @@ class FashionMagazine extends Component {
                     <TitleCardGrid titleCards={this.state.titleCards} />
                 </div>
                 <div className={classes.RightRailing}>
-
+                    <div className={classes.HeadlineFeed}>
+                        <div className={classes.FeedHeader}>
+                            <span className={classes.Date}>
+                                JULY 27, 2018
+                            </span>
+                        </div>
+                        <div className={[classes.FeedItem, classes.FeedHero].join(' ')}>
+                            <div className={[classes.FeedHeadline, classes.HeroHeadline].join(' ')}>
+                                <h2>The Star Wars: Episode XI Cast is Full of Big Surprises</h2>
+                                <span>Welcome back, Lando and... Luke?</span>
+                                <div className={classes.Timestamp}>
+                                    <span><i className="fas fa-align-left" /> 2 HOURS AGO</span>
+                                </div>
+                            </div>
+                            <div className={[classes.FeedImage, classes.HeroImage].join(' ')}>
+                                <img src="https://media.gq.com/photos/5b5b8a2d0c61df5a7d2e2ed4/1:1/w_120/starwars-episode-iX-cast-announcement-gq.jpg" alt="Hero Image" />
+                            </div>
+                        </div>
+                        <div className={classes.FeedItem}>
+                            <div className={classes.FeedImage}>
+                                <img src="https://media.gq.com/photos/5b5b7cac821b2c6074eaeb9d/1:1/w_80/Stop-Oily-Skin-GQ-July-2018-072718-3x2.jpg" alt="Feed Item Image" />
+                            </div>
+                            <div className={classes.FeedHeadline}>
+                                <h2>How to Fix Greasy Skin So Your Face Doesn't Shine in Photos</h2>
+                                <span>It's actually not as hard as you think</span>
+                                <div className={classes.Timestamp}>
+                                    <span><i className="fas fa-align-left" /> 2 HOURS AGO</span>
+                                </div>
+                            </div>
+                        </div>
+                        <div className={classes.FeedItem}>
+                            <div className={classes.FeedImage}>
+                                <img src="https://media.gq.com/photos/5b5b7cac821b2c6074eaeb9d/1:1/w_80/Stop-Oily-Skin-GQ-July-2018-072718-3x2.jpg" alt="Feed Item Image" />
+                            </div>
+                            <div className={classes.FeedHeadline}>
+                                <h2>How to Fix Greasy Skin So Your Face Doesn't Shine in Photos</h2>
+                                <span>It's actually not as hard as you think</span>
+                                <div className={classes.Timestamp}>
+                                    <span><i className="fas fa-align-left" /> 2 HOURS AGO</span>
+                                </div>
+                            </div>
+                        </div>
+                        <div className={classes.FeedItem}>
+                            <div className={classes.FeedImage}>
+                                <img src="https://media.gq.com/photos/5b5b7cac821b2c6074eaeb9d/1:1/w_80/Stop-Oily-Skin-GQ-July-2018-072718-3x2.jpg" alt="Feed Item Image" />
+                            </div>
+                            <div className={classes.FeedHeadline}>
+                                <h2>How to Fix Greasy Skin So Your Face Doesn't Shine in Photos</h2>
+                                <span>It's actually not as hard as you think</span>
+                                <div className={classes.Timestamp}>
+                                    <span><i className="fas fa-align-left" /> 2 HOURS AGO</span>
+                                </div>
+                            </div>
+                        </div>
+                        <div className={classes.FeedFooter}>
+                            <span>View More Posts<i className="fas fa-arrow-right" /></span>
+                        </div>
+                    </div>
                 </div>
             </div>
         );
