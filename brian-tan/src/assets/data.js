9f90b658c18581b9676b1be02d6279f6dfb7b614
Brian Tan <britan3333@gmail.com>
2018-07-30 23:53:58 -0400

    concepts/ga-magazine: staged the site with better images and fake headlines.

--- brian-tan/src/assets/data.js
+++ brian-tan/src/assets/data.js
@@ -1,21 +1,21 @@
 export const fashionMagazineData = {
     banner: {
-        image_url: 'https://images.pexels.com/photos/875722/pexels-photo-875722.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+        image_url: 'https://images.pexels.com/photos/862517/pexels-photo-862517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
         title: 'Most Popular',
-        headline: 'Tips to Off Denim Year Round.',
+        headline: 'Pull Off Denim Year Round',
         url: 'https://www.gq.com/?us_site=y'
     },
     featureItems: [
         {
             _id: 'item1',
-            image_url: 'https://media.gq.com/photos/5b5632b82a69b6291675b11d/1:1/w_80/comic-con-style-2018-lede.jpg',
-            text: 'The Best and Wildest Menswear Fits from San Diego Comic-Con 2018',
+            image_url: 'https://images.pexels.com/photos/362168/pexels-photo-362168.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+            text: 'Paul Jaques\'s path from Dishwasher to Oscar Winner',
             url: 'https://www.gq.com/gallery/san-diego-comic-con-2018-style'
         },
         {
             _id: 'item2',
-            image_url: 'https://media.gq.com/photos/5b5632b82a69b6291675b11d/1:1/w_80/comic-con-style-2018-lede.jpg',
-            text: 'The Best and Wildest Menswear Fits from San Diego Comic-Con 2018',
+            image_url: 'https://images.pexels.com/photos/937481/pexels-photo-937481.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+            text: 'P. Gooden\'s Massive Suit Sale is Here',
             url: 'https://www.gq.com/gallery/san-diego-comic-con-2018-style'
         }
     ],
@@ -23,83 +23,83 @@ export const fashionMagazineData = {
         {
             _id: 'titleCard1',
             url: 'https://www.gq.com/story/underrated-exercises-according-to-personal-trainers',
-            image_url: 'https://media.gq.com/photos/5b575cbc694b552c7cc41a7c/3:4/w_240/Whats-My-Rating-GQ-July-2018-072418.jpg',
-            category: 'Really Long Category',
-            headline: 'This Is What Happened When I Asked My Friends to Rate Me',
-            creator: 'CLAY SKIPPER'
+            image_url: 'https://images.pexels.com/photos/46219/people-man-model-glasses-46219.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+            category: 'Style',
+            headline: 'Justin Chen is Low Key Making "Tourist Style" a Thing',
+            creator: 'KENNY PEPPERS'
         },
         {
             _id: 'titleCard2',
             url: 'https://www.gq.com/story/underrated-exercises-according-to-personal-trainers',
-            image_url: 'https://media.gq.com/photos/5b59fb3bebe0f9125a9fbb4f/3:4/w_240/Screen%20Shot%202018-07-26%20at%2012.46.35%20PM.png',
-            category: 'Sneakers',
-            headline: 'Off-White\'s Nike Air Presto Is Back and More Hyped Than Ever',
-            creator: 'CLAY SKIPPER'
+            image_url: 'https://images.pexels.com/photos/865580/pexels-photo-865580.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+            category: 'Style',
+            headline: 'Jake Hanson Will Make You Believe in the Turtleneck',
+            creator: 'IAN JEQUOIA'
         },
         {
             _id: 'titleCard3',
             url: 'https://www.gq.com/story/underrated-exercises-according-to-personal-trainers',
-            image_url: 'https://media.gq.com/photos/5b5a2662aa6bf94608b2597c/3:4/w_240/Underrated-Exercises-GQ-2018-072618.jpg',
-            category: 'Fitness',
-            headline: 'The Gym\'s 3 Most Underrated Exercises, According to Personal Trainers',
-            creator: 'CLAY SKIPPER'
+            image_url: 'https://images.pexels.com/photos/5928/salad-healthy-diet-spinach.jpg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+            category: 'Dining',
+            headline: 'Convince Your Friends You Can Cook: 5 Meals for this Week',
+            creator: 'WAWA JENKINS'
         },
         {
             _id: 'titleCard4',
             url: 'https://www.gq.com/story/underrated-exercises-according-to-personal-trainers',
-            image_url: 'https://media.gq.com/photos/5b575cbc694b552c7cc41a7c/3:4/w_240/Whats-My-Rating-GQ-July-2018-072418.jpg',
-            category: 'Really Long Category',
-            headline: 'This Is What Happened When I Asked My Friends to Rate Me',
-            creator: 'CLAY SKIPPER'
+            image_url: 'https://images.pexels.com/photos/213165/pexels-photo-213165.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+            category: 'Vehicles',
+            headline: 'This Major Car Company Had Something to Hide',
+            creator: 'KELLI BLUBUK'
         },
         {
             _id: 'titleCard5',
             url: 'https://www.gq.com/story/underrated-exercises-according-to-personal-trainers',
-            image_url: 'https://media.gq.com/photos/5b59fb3bebe0f9125a9fbb4f/3:4/w_240/Screen%20Shot%202018-07-26%20at%2012.46.35%20PM.png',
-            category: 'Sneakers',
-            headline: 'Off-White\'s Nike Air Presto Is Back and More Hyped Than Ever',
-            creator: 'CLAY SKIPPER'
+            image_url: 'https://images.pexels.com/photos/794064/pexels-photo-794064.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+            category: 'Icons',
+            headline: 'A Saturday With Fractures Star Cheri King',
+            creator: 'JIAN KOO'
         },
         {
             _id: 'titleCard6',
             url: 'https://www.gq.com/story/underrated-exercises-according-to-personal-trainers',
-            image_url: 'https://media.gq.com/photos/5b5a2662aa6bf94608b2597c/3:4/w_240/Underrated-Exercises-GQ-2018-072618.jpg',
-            category: 'Fitness',
-            headline: 'The Gym\'s 3 Most Underrated Exercises, According to Personal Trainers',
-            creator: 'CLAY SKIPPER'
+            image_url: 'https://images.pexels.com/photos/279225/pexels-photo-279225.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+            category: 'Travel',
+            headline: 'Penny Wong Has Been Traveling for Over 40 Years: Here are his Favorite Destinations',
+            creator: 'RANDAL JOSEPH'
         }
     ],
     headlineFeedItems: [
         {
             _id: 'feedItem1',
             url: 'https://www.gq.com/story/the-star-wars-episode-xi-cast-is-full-of-big-surprises',
-            image_url: 'https://media.gq.com/photos/5b5b8a2d0c61df5a7d2e2ed4/1:1/w_120/starwars-episode-iX-cast-announcement-gq.jpg',
-            headline: 'The Star Wars: Episode XI Cast is Full of Big Surprises',
-            description: 'Welcome back, Lando and... Luke?',
+            image_url: 'https://images.pexels.com/photos/705821/pexels-photo-705821.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+            headline: 'Giardiniera\'s New Album "Rex" is Full of Surprises',
+            description: 'Repeat listens incoming...',
             time_published: 2
         },
         {
             _id: 'feedItem2',
             url: 'https://www.gq.com/story/how-to-fix-greasy-skin',
-            image_url: 'https://media.gq.com/photos/5b5b7cac821b2c6074eaeb9d/1:1/w_80/Stop-Oily-Skin-GQ-July-2018-072718-3x2.jpg',
-            headline: 'How to Fix Greasy Skin So Your Face Doesn\'t Shine in Photos',
-            description: 'It\'s actually not as hard as you think',
+            image_url: 'https://images.pexels.com/photos/539432/pexels-photo-539432.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+            headline: 'Coffee is Very Good for You Apparently',
+            description: 'yeppers\' Senior Food Editor Wanda Kyle has some good news.',
             time_published: 2
         },
         {
             _id: 'feedItem3',
             url: 'https://www.gq.com/story/how-to-fix-greasy-skin',
-            image_url: 'https://media.gq.com/photos/5b5b7cac821b2c6074eaeb9d/1:1/w_80/Stop-Oily-Skin-GQ-July-2018-072718-3x2.jpg',
-            headline: 'How to Fix Greasy Skin So Your Face Doesn\'t Shine in Photos',
-            description: 'It\'s actually not as hard as you think',
+            image_url: 'https://images.pexels.com/photos/157966/person-young-man-beard-emotions-157966.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+            headline: 'Ronny Abadi Has a New Comedy Special',
+            description: 'Prepare for a lot of fart jokes, but wait they are good.',
             time_published: 2
         },
         {
             _id: 'feedItem4',
             url: 'https://www.gq.com/story/how-to-fix-greasy-skin',
-            image_url: 'https://media.gq.com/photos/5b5b7cac821b2c6074eaeb9d/1:1/w_80/Stop-Oily-Skin-GQ-July-2018-072718-3x2.jpg',
-            headline: 'How to Fix Greasy Skin So Your Face Doesn\'t Shine in Photos',
-            description: 'It\'s actually not as hard as you think',
+            image_url: 'https://images.pexels.com/photos/6633/car-superhero-symbol-batman.jpg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+            headline: 'Casting Has Begun for Yet Another Batman',
+            description: 'Who will wear the cowl next?',
             time_published: 2
         },
     ],
@@ -113,58 +113,58 @@ export const fashionMagazineData = {
             {
                 _id: 'titleCard1',
                 url: 'https://www.gq.com/story/underrated-exercises-according-to-personal-trainers',
-                image_url: 'https://media.gq.com/photos/5b575cbc694b552c7cc41a7c/3:4/w_240/Whats-My-Rating-GQ-July-2018-072418.jpg',
-                category: 'Really Long Category',
-                headline: 'This Is What Happened When I Asked My Friends to Rate Me',
-                creator: 'CLAY SKIPPER'
+                image_url: 'https://images.pexels.com/photos/404168/pexels-photo-404168.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+                category: 'Style',
+                headline: '10 Shoes Over $700 You Must Own',
+                creator: 'ANTHONY TRACY KARR'
             },
             {
                 _id: 'titleCard2',
                 url: 'https://www.gq.com/story/underrated-exercises-according-to-personal-trainers',
-                image_url: 'https://media.gq.com/photos/5b59fb3bebe0f9125a9fbb4f/3:4/w_240/Screen%20Shot%202018-07-26%20at%2012.46.35%20PM.png',
-                category: 'Sneakers',
-                headline: 'Off-White\'s Nike Air Presto Is Back and More Hyped Than Ever',
-                creator: 'CLAY SKIPPER'
+                image_url: 'https://images.pexels.com/photos/423364/pexels-photo-423364.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+                category: 'Style',
+                headline: 'Ben Pelorma Has a Suit Cheat Sheet For You',
+                creator: 'HALEY JENKINS'
             },
             {
                 _id: 'titleCard3',
                 url: 'https://www.gq.com/story/underrated-exercises-according-to-personal-trainers',
-                image_url: 'https://media.gq.com/photos/5b5a2662aa6bf94608b2597c/3:4/w_240/Underrated-Exercises-GQ-2018-072618.jpg',
-                category: 'Fitness',
-                headline: 'The Gym\'s 3 Most Underrated Exercises, According to Personal Trainers',
-                creator: 'CLAY SKIPPER'
+                image_url: 'https://images.pexels.com/photos/428340/pexels-photo-428340.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+                category: 'Style',
+                headline: 'This Designer is the Only One You Will Want to Wear This Summer',
+                creator: 'JAMES DENG'
             },
             {
                 _id: 'titleCard4',
                 url: 'https://www.gq.com/story/underrated-exercises-according-to-personal-trainers',
-                image_url: 'https://media.gq.com/photos/5b575cbc694b552c7cc41a7c/3:4/w_240/Whats-My-Rating-GQ-July-2018-072418.jpg',
-                category: 'Really Long Category',
-                headline: 'This Is What Happened When I Asked My Friends to Rate Me',
-                creator: 'CLAY SKIPPER'
+                image_url: 'https://images.pexels.com/photos/941693/pexels-photo-941693.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+                category: 'Life Style',
+                headline: 'How Jeof Isengoud Finds Positivity At Every Turn',
+                creator: 'RONALD DOCKERY'
             },
             {
                 _id: 'titleCard5',
                 url: 'https://www.gq.com/story/underrated-exercises-according-to-personal-trainers',
-                image_url: 'https://media.gq.com/photos/5b59fb3bebe0f9125a9fbb4f/3:4/w_240/Screen%20Shot%202018-07-26%20at%2012.46.35%20PM.png',
-                category: 'Sneakers',
-                headline: 'Off-White\'s Nike Air Presto Is Back and More Hyped Than Ever',
-                creator: 'CLAY SKIPPER'
+                image_url: 'https://images.pexels.com/photos/175687/pexels-photo-175687.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+                category: 'Accessories',
+                headline: 'It Might Be 100 Degrees Outside But You Can Still Rock These Hats',
+                creator: 'FRANK LUNDEN'
             },
             {
                 _id: 'titleCard6',
                 url: 'https://www.gq.com/story/underrated-exercises-according-to-personal-trainers',
-                image_url: 'https://media.gq.com/photos/5b5a2662aa6bf94608b2597c/3:4/w_240/Underrated-Exercises-GQ-2018-072618.jpg',
-                category: 'Fitness',
-                headline: 'The Gym\'s 3 Most Underrated Exercises, According to Personal Trainers',
-                creator: 'CLAY SKIPPER'
+                image_url: 'https://images.pexels.com/photos/865920/pexels-photo-865920.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+                category: 'Style',
+                headline: 'Stop Looking Dorky Behind the Camera',
+                creator: 'LENNY TZU'
             },
             {
                 _id: 'titleCard7',
                 url: 'https://www.gq.com/story/underrated-exercises-according-to-personal-trainers',
-                image_url: 'https://media.gq.com/photos/5b575cbc694b552c7cc41a7c/3:4/w_240/Whats-My-Rating-GQ-July-2018-072418.jpg',
-                category: 'Really Long Category',
-                headline: 'This Is What Happened When I Asked My Friends to Rate Me',
-                creator: 'CLAY SKIPPER'
+                image_url: 'https://images.pexels.com/photos/977796/pexels-photo-977796.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+                category: 'Music',
+                headline: 'WoWo Ma: "I am the Face of Jumble Rap"',
+                creator: 'KENNY PEPPERS'
             },
         ]
     }
