f7ddcbc7d953771bf1750c7cf3120d34f037c200
Brian Tan <britan3333@gmail.com>
2018-09-27 18:34:32 -0400

    concepts/new-landing-page: created a new simplistic home screen where users can preview my projects with a nice video in the background.

--- brian-tan/src/container/Home/Home.js
+++ brian-tan/src/container/Home/Home.js
@@ -1,34 +1,112 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
-import sharedClasses from '../../index.scss';
-import classes from './Home.scss';
-import TiledBanner from '../../component/BannerTemplates/TiledBanner/TiledBanner';
-import ramenIcon from '../../assets/images/ramen.png';
-import Aux from '../../hoc/Aux/Aux';
-import YelpSearch from '../Food/YelpSearch/YelpSearch';
-import YelpBusinessGrid from '../Food/YelpBusinessGrid/YelpBusinessGrid';
-import SavedRestaurantsMap from '../Food/FoodMap/FoodMap';
-import TopRestaurants from '../Food/TopRestaurants/TopRestaurants';
+import classes from './Home.scss'
+import digaaiVideo from '../../assets/videos/digaaiVideo.mp4';
+import foodJournalVideo from '../../assets/videos/foodJournalVideo.mp4';
 
 class Home extends Component {
-    render () {
-        return (
-            <Aux>
-                <div className={sharedClasses.Banner}>
-                    <TiledBanner image={ramenIcon} />
-                </div>
-                <div className={classes.HitListSection}>
-                    <div className={classes.HitListTitle}>
-                        <span>Restaurant Bookmarks</span>
-                    </div>
-                    <YelpSearch />
-                    <YelpBusinessGrid />
-                </div>
-                <SavedRestaurantsMap />
-                <TopRestaurants />
-            </Aux>
-        );
+    constructor (props) {
+        super(props);
+
+        this.digaaiVidRef = null;
+        this.setDigaaiVidRef = element => {
+            this.digaaiVidRef = element;
+        };
+
+        this.foodJournalVidRef = null;
+        this.setFoodJournalVidRef = element => {
+            this.foodJournalVidRef = element;
+        };
+
+        this.activateVideo = this.activateVideo.bind(this);
+        this.deactivateVideo = this.deactivateVideo.bind(this);
     }
+
+    state = {
+        videosConfig: {
+            digaaiVid: {
+                isActive: false
+            },
+            foodJournalVid: {
+                isActive: false
+            }
+        }
+    }
+
+    activateVideo(ref, vidName) {
+        let updatedVideosConfig = {...this.state.videosConfig};
+        updatedVideosConfig[vidName].isActive = true;
+
+        this.setState({
+            videosConfig: updatedVideosConfig
+        });
+
+        ref.play();
+    }
+
+    deactivateVideo(ref, vidName) {
+        let updatedVideosConfig = {...this.state.videosConfig};
+        updatedVideosConfig[vidName].isActive = false;
+
+        this.setState({
+            videosConfig: updatedVideosConfig
+        });
+
+        ref.pause();
+    }
+
+    render () {
+        let digaaiVideoElement = (
+            <div className={classes.ProjectVideo}>
+                <video className={this.state.videosConfig.digaaiVid.isActive ? classes.ActiveVideo : null} ref={this.setDigaaiVidRef} loop muted>
+                    <source src={digaaiVideo} type="video/mp4" />
+                </video>
+            </div>
+        );
+
+        let foodJournalVideoElement = (
+            <div className={classes.ProjectVideo}>
+                <video className={this.state.videosConfig.foodJournalVid.isActive ? classes.ActiveVideo : null} ref={this.setFoodJournalVidRef} loop muted>
+                    <source src={foodJournalVideo} type="video/mp4" />
+                </video>
+            </div>
+        );
+
+        return (
+            <div className={classes.Home}>
+                <div className={classes.VideoLayer}>
+                    {digaaiVideoElement}
+                    {foodJournalVideoElement}
+                </div>
+                <div className={classes.DetailLayer}>
+                    <div className={classes.HomeHeader}>
+                        <h1>BRIAN TAN</h1>
+                        <span>Software Developer</span>
+                    </div>
+                    <div className={classes.ProjectContainer}>
+                        <h3>Projects</h3>
+                        <ul className={classes.ProjectList}>
+                            <li>
+                                <span onMouseEnter={() => this.activateVideo(this.digaaiVidRef, 'digaaiVid')}
+                                    onMouseLeave={() => this.deactivateVideo(this.digaaiVidRef, 'digaaiVid')}>
+                                    digaai
+                                </span>
+                            </li>
+                            <li>
+                                <span onMouseEnter={() => this.activateVideo(this.foodJournalVidRef, 'foodJournalVid')} 
+                                    onMouseLeave={() => this.deactivateVideo(this.foodJournalVidRef, 'foodJournalVid')}>
+                                    food journal
+                                </span>
+                            </li>
+                            <li><span>inqueens</span></li>
+                        </ul>
+                    </div>
+                </div>
+
+            </div>
+        );
+    };
 }
 
 export default Home;
